338aa8f93e37c483048d6f09a9ac5e6ed08e5121
KubaPro010 <kuba.pro010@gmail.com>
2025-09-13 22:02:27 +0200

    make jingle rarer

--- radioPlayer.py
+++ radioPlayer.py
@@ -306,7 +306,7 @@ def play_playlist(playlist_path, custom_playlist: bool=False, play_newest_first=
         
         pr = procman.play(track_path, to_fade_in, True)
         print_wait(pr.duration - CROSSFADE_DURATION, 1, pr.duration, f"{track_name}: ")
-        if JINGIEL_FILE and random.choice([False, True, False]):
+        if JINGIEL_FILE and random.choice([False, True, False, False]):
             logger.info("Playing the jingiel")
             time.sleep(CROSSFADE_DURATION / 2)
             procman.play(JINGIEL_FILE, False, False).process.wait()
