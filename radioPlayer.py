4465fdb1f7c3d1a4dea35fe0a15f73daef4020dd
KubaPro010 <kuba.pro010@gmail.com>
2025-09-18 20:20:27 +0200

    prevent the chimer if it played last time

--- radioPlayer.py
+++ radioPlayer.py
@@ -273,6 +273,7 @@ def play_playlist(playlist_path, custom_playlist: bool=False, play_newest_first=
         if do_shuffle: random.shuffle(tracks)
 
     return_pending = False
+    last_chimed = False
     
     to_fade_in = False
 
@@ -306,12 +307,14 @@ def play_playlist(playlist_path, custom_playlist: bool=False, play_newest_first=
         
         pr = procman.play(track_path, to_fade_in, True)
         print_wait(pr.duration - CROSSFADE_DURATION, 1, pr.duration, f"{track_name}: ")
-        if JINGIEL_FILE and random.choice([False, True, False, False]):
+        if JINGIEL_FILE and random.choice([False, True, False, False]) and not last_chimed:
             logger.info("Playing the jingiel")
             procman.play(JINGIEL_FILE, False, False).process.wait()
             to_fade_in = False
+            last_chimed = True
         else:
             to_fade_in = True
+            last_chimed = False
 
 def can_delete_file(filepath):
     if not os.path.isfile(filepath): return False
@@ -427,7 +430,7 @@ def main():
                     logger.info("Reload requested, restarting with new arguments...")
                     result = "reload"
 
-                if result == "reload":  play_loop = False
+                if result == "reload": play_loop = False
     except Exception as e:
         logger.error(f"Unexpected error: {e}")
         procman.stop_all()
