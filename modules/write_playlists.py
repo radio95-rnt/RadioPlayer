ca3e8522f8aff0b5895b07201b6af8bc015b758a
Kuba <132459354+KubaPro010@users.noreply.github.com>
2025-10-11 17:01:56 +0200

    this is fucked

--- modules/write_playlists.py
+++ modules/write_playlists.py
@@ -1,10 +1,8 @@
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    class PlayerModule:
-        def on_new_playlist(self, playlist: list[tuple[str, bool, bool, bool]]):
-            pass
-        def on_new_track(self, index: int, track: str, to_fade_in: bool, to_fade_out: bool, official: bool):
-            pass
+class PlayerModule:
+    def on_new_playlist(self, playlist: list[tuple[str, bool, bool, bool]]):
+        pass
+    def on_new_track(self, index: int, track: str, to_fade_in: bool, to_fade_out: bool, official: bool):
+        pass
 
 class Module(PlayerModule):
     def __init__(self) -> None:
