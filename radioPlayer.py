550ed91157e64778c61e167b2045b8bc6ed765fd
Kuba <132459354+KubaPro010@users.noreply.github.com>
2025-10-13 20:20:19 +0200

    hang on no

--- radioPlayer.py
+++ radioPlayer.py
@@ -227,6 +227,7 @@ def play_playlist(playlist_path):
             procman.wait_all()
             return
         
+        track_tuple = playlist[i]
         if active_modifier: 
             track_tuple = active_modifier.play(i, track_tuple)
             modified = True
@@ -261,6 +262,7 @@ def play_playlist(playlist_path):
 
         if official: print_wait(ttw, 1, pr.duration, f"{track_name}: ")
         else: time.sleep(ttw)
+        i += 1
 
 def main():
     global playlist_advisor, active_modifier
